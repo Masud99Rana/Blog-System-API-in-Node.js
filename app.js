e1d652ad7b5abaa3b9d47d1dfcb432c515ac7f37
Masud99Rana <Masudrana917775@gmail.com>
2020-02-02 10:22:04 +0600

    CORS allowed

--- app.js
+++ app.js
@@ -7,5 +7,14 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');// 'codepen.id' or, 'codepen.id, w3.io'
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+}); // must declare this before using any route
+
+app.use('/feed', feedRoutes); 
+
 app.listen(8000);
 console.log('Hey, I am here');
